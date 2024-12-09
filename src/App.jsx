a1348502fccd327239e3bdde4f78b155c28d7dd6
Swapnil Katwe <swapnil.katwe111@gmail.com>
2024-12-09 12:37:29 +0100

    Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import logoImage from "./assets/investment-calculator-logo.png";
+function App() {
+  return (
+    <main id="header">
+      <div>
+        <img src={logoImage} />
+        <h1>Investment Calculator</h1>
+      </div>
+
+      <div id="user-input">
+
+      </div>
+    </main>
+  );
+}
+
+export default App;
